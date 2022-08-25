23fbde61eb4ff62d90d13617a70d80c4e90cc479
Vinny Algaron <vinny-algaron93@hotmail.fr>
2022-08-25 17:51:42 +0200

    add all rentals need to add all MY rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -17,6 +17,11 @@ class RentalsController < ApplicationController
     @rental = Rental.find(params[:id])
   end
 
+  def index
+    @rentals = Rental.all
+
+  end
+
   private
 
   def rental_params
